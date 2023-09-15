8b0f5b2f7d5967210f3efaa2b210bbb88f7b6602
Fin <finlayjhoughton@gmail.com>
2023-09-15 16:43:14 +0100

    changes

--- src/main.jl
+++ src/main.jl
@@ -18,17 +18,17 @@ const EDGE::Float64 = 10 * DIST
 
 const X_LIMITS::Tuple{Float64, Float64} = (-EDGE, EDGE)
 const Y_LIMITS::Tuple{Float64, Float64} = (-EDGE, EDGE)
-
+ 
 mutable struct Particle
     mass::Float64
     pos::SVector{2, Float64}
-    v::Vector{Float64}
-    force_applied::Vector{Float64}
+    v::SVector{2, Float64}
+    force_applied::SVector{2, Float64}
     fixed::Bool
     function Particle(
         mass::Float64,
         pos::SVector{2, Float64},
-        v::Vector{Float64}=zeros(Float64, 2); 
+        v::SVector{2, Float64}=zeros(Float64, 2); 
         fixed::Bool=false
     )::Particle
         if size(v, 1) != 2
@@ -44,14 +44,12 @@ end
 
 function calculate_centre_of_mass(ps::Vector{Particle})::SVector{2, Float64}
     total_mass::Float64 = 0
-    total_x::Float64 = 0
-    total_y::Float64 = 0
+    total::SVector{2, Float64} = SVector(0, 0)
     for p ∈ ps
         total_mass += p.mass
-        total_x += p.pos[1]
-        total_y += p.pos[2]
+        total += p.pos
     end
-    SVector(total_x / total_mass, total_y / total_mass)
+    total / total_mass
 end
 
 struct BHTree
@@ -100,45 +98,6 @@ struct BHTree
         children = [BHTree(quad, quad_centre, half_side_len) for (quad, quad_centre) in zip(quads, centres)]
         Just(new(particles, children..., centre_of_mass, side_length))
     end
-
-    function BHTree(
-        x::Int,
-        particles::Vector{Particle},
-        centre::SVector{2, Float64},
-        side_length::Float64
-    )::Maybe{BHTree}
-        
-        len = length(particles)
-
-        if len == 0
-            return nothing
-        end
-
-        centre_of_mass::SVector{2, Float64} = calculate_centre_of_mass(particles)
-        half_side_len::Float64 = 0.5 * side_length
-        quarter_side_len::Float64 = 0.5 * half_side_len 
-
-        nws::Vector{Particle} = []
-        nes::Vector{Particle} = []
-        sws::Vector{Particle} = []
-        ses::Vector{Particle} = []
-        quads = (nws, nes, sws, ses)
-
-        if length(particles) ≠ 1
-            for particle ∈ particles
-                push_to_vector!(quads..., particle, centre)
-            end
-        end
-
-        centres = (
-            centre + SVector{2, Float64}(-quarter_side_len, quarter_side_len),
-            centre + SVector{2, Float64}(quarter_side_len, quarter_side_len), 
-            centre + SVector{2, Float64}(-quarter_side_len, -quarter_side_len),
-            centre + SVector{2, Float64}(quarter_side_len, -quarter_side_len))
-
-        children = [BHTree(quad, quad_centre, half_side_len) for (quad, quad_centre) in zip(quads, centres)]
-        Just(new(particles, children..., centre_of_mass, side_length))
-    end
 end
 
 function push_to_vector!(
@@ -164,7 +123,7 @@ end
 function random_particle() :: Particle
     m::Float64 = M_EARTH * (rand() + 0.5)
     pos::SVector{2, Float64} = SVector{2, Float64}(rand() - 0.5, rand() - 0.5) * DIST * 4
-    v::Vector{Float64} = (rand(Float64, 2) * 2 - ones(2)) * DIST * 0.5
+    v::SVector{2, Float64} = SVector{2, Float64}(rand(Float64, 2) * 2 - ones(2)) * DIST * 0.5
     Particle(m, pos, v)
 end
 
