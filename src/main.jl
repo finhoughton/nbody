2dafb33f66e6bc9463b4638e5113b6e5363507c4
Fin <finlayjhoughton@gmail.com>
2023-10-09 09:33:23 +0100

    changes

--- src/main.jl
+++ src/main.jl
@@ -11,9 +11,9 @@ const M_EARTH::Float64 = 6e22
 const DIST::Float64 = 1e9
 
 # const G::Float64 = 6.674e-11
-const G::Float64 = 30
+const G::Float64 = 3
 
-const EPS_SOFTENING::Float64 = 1e7
+const EPS_SOFTENING::Float64 = 1e10
 # stop forcess becoming too big when objects are very close
 
 const EDGE::Float64 = 10 * DIST
@@ -24,7 +24,7 @@ const Y_LIMITS::Tuple{Float64, Float64} = (-EDGE, EDGE)
 function random_particle() :: Particle
     m::Float64 = M_EARTH * (rand() + 0.5)
     pos = SVector{2, Float64}(rand() - 0.5, rand() - 0.5) * DIST * 4
-    v = SVector{2, Float64}(rand() - 0.5, rand() - 0.5) * DIST * 0.5
+    v = SVector{2, Float64}(rand() - 0.5, rand() - 0.5) * DIST * 0.2
     Particle(m, pos, v)
 end
 
@@ -72,12 +72,11 @@ end
 
 function main()::Nothing
     particles::Vector{Particle} = [random_particle() for _ ∈ 1:100]
-    root = unsafe_from_just(BHTree(particles, SVector{2, Float64}(0, 0), EDGE))
     t::Float64 = 0
     Δt::Float64 = 1/32
-    while true
+    while !isempty(particles)
         start::DateTime = now()
-        root = unsafe_from_just(BHTree(particles, SVector{2, Float64}(0, 0), EDGE))
+        root = unsafe_from_just(BHTree(particles, SVector{2, Float64}(0, 0), 2 * EDGE))
         step!(particles, root, Δt)
         filter!(p -> maximum(abs, p.pos) < EDGE, particles) # delete offscreen particles
         showparticles(particles)
