ef2c54705df334349578f4dedffbae416a78f605
Fin <finlayjhoughton@gmail.com>
2023-11-07 09:20:16 +0000

    moved constants

--- src/main.jl
+++ src/main.jl
@@ -5,34 +5,13 @@ using Plots
 using Random
 gr()
 
-include("barnes-hut.jl")
+include("iteration-algorithms.jl")
 
 # order of include():
 # main
 # barnes-hut
 # particle
 # utils
-
-# ----- constants -----
-
-const FPS::Int64 = 50
-
-const Δt::Float64 = 1/FPS
-
-const M_EARTH::Float64 = 6e22
-
-const DIST::Float64 = 1e9
-
-# const G::Float64 = 6.674e-11
-const G::Float64 = 30
-
-const EPS_SOFTENING::Float64 = 1e8
-# stop forcess becoming too big when objects are very close
-
-const EDGE::Float64 = 10 * DIST
-
-const X_LIMITS::Tuple{Float64, Float64} = (-EDGE, EDGE)
-const Y_LIMITS::Tuple{Float64, Float64} = (-EDGE, EDGE)
 
 # ------ displaying ------
 
